68e6688fe6670e92b6bd1d68138c70b0734cd9ff
Arkan <arkankhan051@gmail.com>
2024-04-10 20:39:24 +0530

    Added component for committeesName reusability

---
+++ src/components/CommitteesName.jsx
@@ -0,0 +1,14 @@
+export function CommitteesName({ name, logo }) {
+  return (
+    <div className="flex items-center justify-center w-full sm:w-1/2 mb-4 md:mb-8"> {/* Responsive width for mobile */}
+      <div className="flex items-center justify-center rounded-3xl bg-white p-4 sm:w-40 sm:h-20"> {/* Responsive size for mobile */}
+        <div className="flex justify-center items-center w-12 h-12 md:w-16 md:h-16 rounded-full bg-gray-300 mr-2"> {/* Placeholder for logo */}
+          {/* Placeholder for logo */}
+        </div>
+        <div className="flex flex-col justify-center">
+          <h3 className="text-lg font-semibold text-center">{name}</h3> {/* Center-aligned name */}
+        </div>
+      </div>
+    </div>
+  );
+}
